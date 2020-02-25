bea1ee81958a4d6166e8c81bacc6ed2a3a64eff1
xNombre <kartapolska@gmail.com>
2020-02-25 22:46:24 +0100

    mido: Build missing camera library
    This fixes:
    "ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.common@1.0.so" not found"
    
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- device.mk
+++ device.mk
@@ -138,6 +138,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.common@1.0 \
     camera.msm8953 \
     libmm-qcamera \
     Snap
