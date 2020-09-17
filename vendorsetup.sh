297cdd2921f3d462d097fd6f990f555b517c2b4e
Sagarrokade006 <sagarrokade006@gmail.com>
2020-09-17 10:09:04 +0000

    Track Hals

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+rm -rf hardware/qcom-caf/msm8996/display && git clone https://github.com/smokey18/hardware_qcom_display hardware/qcom-caf/msm8996/display
+
+rm -rf hardware/qcom-caf/msm8996/media && git clone https://github.com/smokey18/hardware_qcom_media hardware/qcom-caf/msm8996/media
+
+rm -rf hardware/qcom-caf/msm8996/audio && git clone https://github.com/smokey18/hardware_qcom_audio hardware/qcom-caf/msm8996/audio
