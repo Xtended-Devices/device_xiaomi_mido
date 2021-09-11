94b8f9e14169bbccc85d211c528b2bf78dca8c5b
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-09-11 14:46:47 +0000

    mido: track MIUI Camera from device.mk
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ PRODUCT_USES_QCOM_HARDWARE := true
 
 OVERRIDE_QCOM_HARDWARE_VARIANT := msm8996-r
 
+# Inherit MIUI Camera Port
+$(call inherit-product, vendor/xiaomi/MiuiCamera/config.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
